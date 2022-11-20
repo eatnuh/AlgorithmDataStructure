028b8f70ee1dd46ef3ca807591070e142a7df8f2
eatnuh <eatnuh@gmail.com>
2022-11-20 19:02:37 +0900

    add 11.20

---
+++ src/main/java/problemsolve/programmers/P42577.java
@@ -0,0 +1,18 @@
+package problemsolve.programmers;
+
+import java.util.Arrays;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/42577
+public class P42577 {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        for(int i = 0; i < phone_book.length - 1; i++) {
+            String before = phone_book[i];
+            String after = phone_book[i + 1];
+
+            if(after.startsWith(before)) return false
+
+        }
+        return true;
+    }
+}
