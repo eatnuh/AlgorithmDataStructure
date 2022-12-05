d0f7a5f0b60cc2660b038e507e32a02930a268d4
eatnuh <eatnuh@gmail.com>
2022-12-05 11:45:00 +0900

    add 12.05

--- src/main/java/interview/Anagram.java
+++ src/main/java/interview/Anagram.java
@@ -1,6 +1,7 @@
 package interview;
 
 import java.util.Arrays;
+
 // 아나그램(같은 알파벳으로 이루어진 문자열) 판별
 public class Anagram {
 
@@ -17,16 +18,16 @@ public class Anagram {
 
     // 2. using bucket O(n)
     public boolean compareAnagramUsingBucket(String str1, String str2) {
-        if(str1.length() != str2.length()) return false;
+        if (str1.length() != str2.length()) return false;
         int len = str1.length();
         int[] bucket = new int[26]; // lower case alphabet
 
-        for(int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) {
             bucket[str1.charAt(i) - 'a']++;
         }
 
-        for(int i = 0; i < len; i++) {
-            if(bucket[str2.charAt(i)]-- < 0) return false;
+        for (int i = 0; i < len; i++) {
+            if (bucket[str2.charAt(i) - 'a']-- < 0) return false;
         }
 
         return true;
